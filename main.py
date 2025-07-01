67bce8dc44ce80b877dda7a7391ced5dc95d6146
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-07-01 17:55:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import math
 col1, col2, col3 = st.columns([1, 2, 1]) 
 
 with col2:
-  dividend=int(st.text_input("Enter your dividend here: "))
-  divisor=int(st.text_input("Enter your dividend here: "))
+  dividend=int(st.text_input("Enter your dividend here: ", value=5))
+  divisor=int(st.text_input("Enter your divisor here: ", value=5))
 
 try:
   st.write(dividend/divisor)
