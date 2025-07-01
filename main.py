2d3fefaaa451cfb9ad1cbb61d3c19a9e480c938a
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-07-01 17:17:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-dividend=int(input("Enter your dividend here: "))
-divisor=int(input("Enter your dividend here: "))
+dividend=int(st.text_input("Enter your dividend here: "))
+divisor=int(st.text_input("Enter your dividend here: "))
 
 try:
   print(dividend/divisor)
