9399f594cddad34ca292fa848b441f2a0a5335e4
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-07-01 18:00:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import math
 col1, col2, col3 = st.columns([1, 2, 1]) 
 
 with col2:
+  st.title("Div Checker")
   dividend=int(st.text_input("Enter your dividend here: ", value=5))
   divisor=int(st.text_input("Enter your divisor here: ", value=5))
 
@@ -12,3 +13,5 @@ try:
   st.write(dividend/divisor)
 except ZeroDivisionError:
   st.write("The divisor cannot be zero!")
+except ValueError:
+  st.write("Make sure to enter in two numbers!")
