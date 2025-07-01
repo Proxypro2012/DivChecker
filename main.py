90fa5baabe472b72ed5254d186368a1391675622
Kabir Tiwari <kabir.tiwari@outlook.com>
2025-07-01 18:04:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 col1, col2, col3 = st.columns([1, 2, 1]) 
 
 with col2:
+    st.title("Div Checker")
     dividend_input = st.text_input("Enter your dividend here:", value="5")
     divisor_input = st.text_input("Enter your divisor here:", value="5")
 
